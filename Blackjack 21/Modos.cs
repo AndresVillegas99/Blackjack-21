f8eb64e1b139d1339ac1fdd5fe776b172a0b27dd
yoela <yoela@DESKTOP-BTI0GIP>
2021-02-26 21:13:27 -0600

    Metodos Light21 y Even21

--- Blackjack 21/Modos.cs
+++ Blackjack 21/Modos.cs
@@ -7,6 +7,37 @@ namespace Blackjack_21
     class Modos
     {
         
+<<<<<<< HEAD
+        //Even 21
+        //Cartas As + Pares.
+        public Baraja Even21(Baraja Bar){ 
+            foreach(Cartas item in Bar.baraja)
+            {
+                if (item.valor < 1 && item.valor / 2 == 0)
+                {
+                    Bar.baraja.Remove(item);
+
+                }
+            }
+            return Bar;
+
+        }
+
+        //Light 21
+        //Cartas Del As al 5
+        public Baraja Light21(Baraja Bar)
+        {
+            foreach (Cartas item in Bar.baraja)
+            {
+                if (item.valor > 1 && item.valor < 5)
+                {
+                    Bar.baraja.Remove(item);
+
+                }
+            }
+            return Bar;
+
+=======
         public Baraja Killer21(Baraja Bar)
         {
             foreach (Cartas item in Bar.baraja)
@@ -29,6 +60,7 @@ namespace Blackjack_21
                 }
             }
             return Bar;
+>>>>>>> ddee79617b7942bc46afd8c16e8787cad7ee353e
         }
     }
 }
