ddee79617b7942bc46afd8c16e8787cad7ee353e
Andres Villegas <villegasa25@gmail.com>
2021-02-26 20:45:58 -0600

    Insertar Killer21 y Odd21

--- Blackjack 21/Modos.cs
+++ Blackjack 21/Modos.cs
@@ -6,5 +6,29 @@ namespace Blackjack_21
 {
     class Modos
     {
+        
+        public Baraja Killer21(Baraja Bar)
+        {
+            foreach (Cartas item in Bar.baraja)
+            {
+                if (item.valor>1 && item.valor< 6)
+                {
+                    Bar.baraja.Remove(item);
+                }
+            }
+            return Bar;
+        }
+
+        public Baraja Odd21(Baraja Bar)
+        {
+            foreach (Cartas item in Bar.baraja)
+            {
+                if (item.valor % 2 == 0)
+                {
+                    Bar.baraja.Remove(item);
+                }
+            }
+            return Bar;
+        }
     }
 }
